b76b19ac54e9314b4af5eabd42c9c2f087dbf8d3
Ondrej Skrdlant <ondrej.skrdlant@quanti.cz>
2019-07-16 15:13:17 +0200

    Initial commit

---
+++ src/main/java/com/cleverlance/restcafe/RestcafeApplication.java
@@ -0,0 +1,13 @@
+package com.cleverlance.restcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcafeApplication.class, args);
+	}
+
+}
